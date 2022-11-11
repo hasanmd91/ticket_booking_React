0b38218365bc57a90a77a3adaef4692955164947
Vijay KC <vj.kc2007@gmail.com>
2022-11-11 19:53:56 +0200

    Data passing complete

--- src/Components/PaymentGateway.jsx
+++ src/Components/PaymentGateway.jsx
@@ -2,29 +2,55 @@ import React from "react";
 import CardImg from "../Assets/card.png";
 import "./PaymentGateway.css";
 
-const PaymentGateway = () => {
+const PaymentGateway = (props) => {
   return (
     <div className="payment-gateway">
       <h2>Enter card details</h2>
       <img src={CardImg} alt="illustration of a bank card" />
       <form className="card-detail">
         <div>
-          <label htmlFor="card-num">Card Number:</label>
-          <input type="number" id="card-num" />
+          <label htmlFor="cardNum">Card Number:</label>
+          <input
+            type="number"
+            id="cardNum"
+            name="cardNum"
+            value={props.cardNum}
+            onChange={props.changeHandler}
+          />
         </div>
 
         <div>
-          <label htmlFor="holder-name">Holder's Name:</label>
-          <input type="text" id="holder-name" />
+          <label htmlFor="holderName">Holder's Name:</label>
+          <input
+            type="text"
+            id="holderName"
+            name="holderName"
+            value={props.holderName}
+            onChange={props.changeHandler}
+          />
         </div>
 
         <div>
-          <label htmlFor="expiry-date">Expirty Date:</label>
-          <input type="tel" id="expiry-date" />
+          <label htmlFor="cardExpiry">Expirty Date:</label>
+          <input
+            type="tel"
+            id="cardExpiry"
+            name="cardExpiry"
+            value={props.cardExpiry}
+            onChange={props.changeHandler}
+          />
         </div>
         <div>
           <label htmlFor="CVC">CVC:</label>
-          <input type="number" id="cvc" />
+          <input
+            type="number"
+            id="cvc"
+            name="cvc"
+            min="100"
+            max="999"
+            value={props.cvc}
+            onChange={props.changeHandler}
+          />
         </div>
         <div>
           <label></label>
