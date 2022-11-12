2bfaa6f589d1184b9499255ff44d836e1c8a0722
Vijay KC <vj.kc2007@gmail.com>
2022-11-12 10:53:11 +0200

    Ticket created and styled

--- src/Components/SampleTicket.jsx
+++ src/Components/SampleTicket.jsx
@@ -4,14 +4,30 @@ import "./SampleTicket.css";
 const SampleTicket = (props) => {
   return (
     <div className="ticket-wrapper">
-      <p>Departure City: {props.departure}</p>
-      <p>Destination City: {props.destination}</p>
-      <p>Travel Date: {props.date}</p>
-      <p>
-        Number of People: Adult: {props.counterAdult} Student:
-        {props.counterStudent} Elderly: {props.counterElderly}
-      </p>
-      <div>Ticket No.: {props.ticketNumber}</div>
+      <div className="cities">
+        <p>{props.departure}</p>
+        <p>&rarr;</p>
+        <p>{props.destination}</p>
+        <p>{props.date}</p>
+      </div>
+
+      <div className="info-wrapper">
+        <div className="personal-info">
+          <p>Name: {props.clientName}</p>
+          <p>Adult:&nbsp; {props.counterAdult}</p>
+          <p>Student: &nbsp;{props.counterStudent}</p>
+          <p>Elderly:&nbsp;{props.counterElderly}</p>
+        </div>
+        <div className="company-info">
+          <h3>Himalayan Express Bus</h3>
+          <p>Phone: 030 4569870</p>
+          <p>Email: himalayan-bus@himalayan.fi</p>
+        </div>
+      </div>
+      <div className="ticket-info">
+        <div>{props.ticketNumber}</div>
+        <div className="ticket-barcode">{props.ticketNumber}</div>
+      </div>
     </div>
   );
 };
