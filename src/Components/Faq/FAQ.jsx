47293ca70f7fd61cbe2f01a70dcec48a3dfc32af
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-12-08 15:03:51 +0200

    FAQ

--- src/Components/Faq/FAQ.jsx
+++ src/Components/Faq/FAQ.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import FaqData from 'react-faq-component';
+
 
 function FAQ() {
   
   const data1 = {
- 
+   "title":"",
    rows: [
    {
     title: 'Is the service disable friendly?',
-    content: 'Himalayan Travels is committed to making travel widely accessible, regardless of ability or disability.',
-    content: 'Himalayan Travels is committed to making travel widely accessible, regardless of ability or disability.',
+    content:
+      `Himalayan Travels is committed to making travel widely accessible, regardless of ability or disability.`,
+          
+  
+    
    },
    {
     title: 'Compensations?',
-    content:`You can apply for compensation using this form in the following cases:
+    content:"You can apply for compensation using this form in the following cases:Your long-distance train journey was delayed by at least 60 minutes.You have a commuter season ticket and the commuter trains are significantly late.The delay caused you extra costs.You or your luggage were damaged during your train journey.",
     
-    Your long-distance train journey was delayed by at least 60 minutes.
-    You have a commuter season ticket and the commuter trains are significantly late.
-    The delay caused you extra costs.
-    You or your luggage were damaged during your train journey.`
    
    },
   ],
