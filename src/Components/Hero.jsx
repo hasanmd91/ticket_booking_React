78af5ad5efd59324e2d0ac811f022f81529dd67e
Vijay KC <vj.kc2007@gmail.com>
2022-12-15 18:05:23 +0200

    Video as background

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,18 +3,25 @@ import { Link } from "react-router-dom";
 
 function Hero() {
   return (
-    <main>
-      <div className="hero h-[350px] bg-hero bg-cover bg-center bg-no-repeat bg w-screen relative">
-        <div className="hero-text absolute translate-x-1/2 translate-y-1/2">
-          <h2 className="text-3xl text-white bg-[#00000070] p-3">
-            Travel Ticket Booking System
-          </h2>
-          <button className="p-4 mt-4">
-            <Link to="/book"> Book Your Ticket Now</Link>
-          </button>
-        </div>
+    <div className="relative h-[50vh] bg-cover bg-center bg-no-repeat bg w-screen">
+      <video
+        className="w-screen h-full object-cover -z-10"
+        autoPlay="autoplay"
+        muted={true}
+        loop={true}
+      >
+        <source src={require("../Assets/bg-video.mp4")}></source>
+      </video>
+
+      <div className="absolute m-auto left-0 right-0 top-1/2 text-center">
+        <h2 className="text-3xl text-white bg-[#00000070] p-4">
+          Travel Ticket Booking System
+        </h2>
+        <button className="p-4 mt-4">
+          <Link to="/book"> Book Your Ticket Now</Link>
+        </button>
       </div>
-    </main>
+    </div>
   );
 }
 
